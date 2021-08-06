5bc543403ec3e2806d9e4e5550eb84039eb418a3
CAG2Mark <markng989@gmail.com>
2021-08-06 16:59:01 +0800

    Countdown page works now

--- script.js
+++ script.js
@@ -1,17 +1,92 @@
 /* jshint esversion:6 */
 
+var daysContainer = document.getElementById("day-value");
+var hoursContainer = document.getElementById("hour-value");
+var minsContainer = document.getElementById("min-value");
+var secsContainer = document.getElementById("sec-value");
+
+var daysText = document.getElementById("day-text");
+var hoursText = document.getElementById("hour-text");
+var minsText = document.getElementById("min-text");
+var secsText = document.getElementById("sec-text");
+
 var eventName;
 var unixTimeUtc;
 
-let setupPage = document.getElementById("setup-page");
-setupPage.style.display = "block";
+// decode url params
+var url = new URL(window.location.href);
+var encoded_json = url.searchParams.get("countdown");
+if (encoded_json) {
+    let timerPage = document.getElementById("timer-page");
+    timerPage.style.display = "block";
+
+    var decoded = JSON.parse(atob(encoded_json));
+    document.getElementById("event-name-value").innerHTML = decoded.eventName;
+    unixTimeUtc = decoded.unix;
+
+    updateTime(Date.now(), unixTimeUtc);
+    
+    // wait x ms
+    let ms = unixTimeUtc % 1000;
+    setTimeout(() => {
+        setInterval(() => updateTime(Date.now(), unixTimeUtc), 1000);
+    }, ms);
+
+}
+else {
+    let setupPage = document.getElementById("setup-page");
+    setupPage.style.display = "block";
+}
+
+// returns [Days, Hours, Minutes, Seconds]
+function updateTime(t1, t2) {
+    let delta = Math.floor((t2 - t1)/1000);
+    if (delta <= 0) return false;
+
+    let days = daysContainer.innerHTML = Math.floor(delta / 86400);
+    delta %= 86400;
+    daysText.innerHTML = days == 1 ? "day" : "days";
+
+    let hours = hoursContainer,innerHTML = Math.floor(delta / 3600);
+    delta %= 3600;
+
+    let mins = minsContainer.innerHTML = Math.floor(delta / 60);
+    delta %= 60;
+
+    let secs = secsContainer.innerHTML = Math.floor(delta);
+
+    return true;
+}
+
 
 function getInput(name) {
     return document.getElementById(name + "-input");
 }
 
+// confirm button event lister
 var confirmButton = document.getElementById("confirm-button");
 confirmButton.addEventListener("click", onConfirm);
+
+// share link event listener
+var shareButton = document.getElementById("share-icon");
+shareButton.addEventListener("click", function () {
+    var shareWrap = document.getElementById("share-link-wrap");
+    var shareContainer = document.getElementById("share-link-container");
+
+    shareWrap.style.display = "inline-block";
+
+    shareContainer.value = window.location.href;
+
+    /* Select the text field */
+    shareContainer.select();
+    shareContainer.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+
+    // display tooltip
+    
+});
 
 // set initial values
 let d_ = new Date();
@@ -39,9 +114,18 @@ function onConfirm() {
     let utcOffset = d.getTimezoneOffset();
     let date = new Date(Date.UTC(parseInt(yr) + 2000, mon-1, day, hr, mn, 0, 0));
 
-    console.log((!isUtc * utcOffset));
-
     let unix = date*1 + (!isUtc * utcOffset * 60000);
+    
+    // create json
+    let obj = {
+        'eventName': eventName,
+        'unix': unix,
+        'createdAt': d*1
+    };
 
-    console.log(unix);
+    let json = JSON.stringify(obj);
+    let encoded = encodeURIComponent(btoa(json));
+
+    let baseurl = window.location.origin+window.location.pathname;
+    window.location.href = baseurl + "?countdown=" + encoded;
 }
